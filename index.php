82c2d05f25d8ae3261c6bcce12817876d56f2586
Heloisa Tonin de L <134729931+heloloisa@users.noreply.github.com>
2023-12-01 14:12:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,67 @@
+<?php 
+
+
+
+
+    $pokemons_api = file_get_contents("https://pokeapi.co/api/v2/pokemon");
+
+    $pokemons = json_decode($pokemons_api, true);
+
+    for ($i=0; $i < 20 ; $i++) { 
+        $pokemon = $pokemons['results'][$i];
+        $todas_infos_api = file_get_contents($pokemon['url']);
+        $pokemons['results'][$i] = json_decode($todas_infos_api, true);
+    }
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pokedex</title>
+    <style>
+        #pesquisa {
+            background : #ca3e1cfd;
+            font-family : Verdana, Geneva , Tahoma, sans-serif;
+            padding :20px;
+            text-align : center;
+
+        }
+        .pokemon {
+            padding : 15px;
+            width :20%;
+            border : solid #000000;
+            float : left;
+            text-align: center;
+            margin: 1% ;
+        }
+        .pokemon img{
+            max-width: 100%;
+            height: 170px;
+        }
+    </style>
 </head>
-    <style> 
-    h1{
-        text-align: center ;
-        font-size: 10em !important;
-    }
-</style>
-
 <body>
-    <?php include 'Navbar.php' ?>
-<div class = "container">
-    <h1 >Index </h1>
+    <div id="pesquisa"> 
+        <form action="logica.php" method="post">
+                <input type="text" placeholder ="Digite o pokemon">
+                <input type="submit" value="Buscar">
+        </form>
+    </div>
+    <div id="pokemons">
+        <?php
+            for ($i=0; $i < 20 ; $i++): ?>
+                <div class="pokemon">
+                    <h1><?= $pokemons['results'][$i]['name']?></h1>
+                    <img src="<?= $pokemons['results'][$i]['sprites']['other']['dream_world']['front_default']?>" alt="Pokemon Dewgong " width="200px">
+                    <p>Altura <?= $pokemons['results'][$i]['sprites']?></p>
+                    <p>Peso <?= $pokemons['results'][$i]['sprites']?></p>
+                    </div>
+            
+    </div>
+          <?php  endfor ?>
 
-</div>
-
+  
 </body>
 </html>
